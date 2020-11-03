00f815d46ad1789ca675f2a6c645019645a6d175
hyunyoung <lhy76927@gmail.com>
2020-11-03 18:10:44 +0900

    커밋 수정

--- 10.Exception/src/exception/ExceptionTest2.java
+++ 10.Exception/src/exception/ExceptionTest2.java
@@ -3,8 +3,16 @@ package exception;
 public class ExceptionTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String date1 = "100";
+		String date2 = "200";
+		
+		int vaue1 = Integer.parseInt(date1);
+		int vaue2 = Integer.parseInt(date2);
+		
+		System.out.println(vaue1+vaue2);
+		
+		System.out.println(vaue1);
+		
 	}
 
 }
